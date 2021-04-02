f33affd2b54e3bf03b7f257ad9a9dbc533ca3e45
Nathan Flurry <developer@nathanflurry.com>
2021-04-02 18:31:19 -0500

    Fix Docker gen permissions

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# npx swagger-cli bundle swagger/swagger.yaml -o dist/swagger.json
+npx swagger-cli bundle swagger/swagger.yaml -o dist/swagger.json
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
-    --skip-validate-spec \
-    --input-spec /local/dist/swagger.json \
-    --generator-name rust \
-    --output /local/client/rust \
-    --additional-properties packageName=nomad-client
+docker run \
+    --rm \
+    --mount "type=bind,src=$(pwd),dst=/local" \
+    --user "$(id -u):$(id -g)" \
+    openapitools/openapi-generator-cli generate \
+        --skip-validate-spec \
+        --input-spec /local/dist/swagger.json \
+        --generator-name rust \
+        --output /local/client/rust \
+        --additional-properties packageName=nomad-client
